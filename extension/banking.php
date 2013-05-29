af5700ba808a2203a5af7d4ed9585730df41ce36
Paul Delbar <paul.delbar@delius.be>
2013-05-29 13:26:21 +0200

    added way of generating/correcting option groups more easily

--- extension/banking.php
+++ extension/banking.php
@@ -24,69 +24,117 @@ function banking_civicrm_xmlMenu(&$files) {
 function banking_civicrm_install() {
   $config = CRM_Core_Config::singleton();
   //create the tables
-  $sql = file_get_contents(dirname( __FILE__ ) .'/sql/banking.sql', true);
+  $sql = file_get_contents(dirname(__FILE__) . '/sql/banking.sql', true);
   CRM_Utils_File::sourceSQLFile($config->dsn, $sql, NULL, true);
-  
+
   //add the required option groups
-  $params = array(
-      'name' => 'civicrm_banking.plugin_types',
-      'version' => 3,
-  );
-  $result = civicrm_api('option_group', 'getsingle', $params);
-  if ($result['is_error']) {
-    $params = array(
-        'version' => 3,
-        'sequential' => 1,
-        'name' => 'civicrm_banking.plugin_types',
-        'is_reserved' => 1,
-        'is_active' => 1,
-        'title' => 'CiviBanking plugin types',
-        'description' => 'The set of possible CiviBanking plugin types',
-        'api.OptionValue.create' => array(
-            array(
-                'label' => 'import',
-                'value' => 1,
-                'is_default' => 0,
-                'is_active' => 1,
-            ),
-            array(
-                'label' => 'match',
-                'value' => 2,
-                'is_default' => 0,
-                'is_active' => 1,
-            ),
-            array(
-                'label' => 'export',
-                'value' => 3,
-                'is_default' => 0,
-                'is_active' => 1,
-            ),
-        ),
-    );
-    $result = civicrm_api('option_group', 'create', $params);    
-  }
-  
-  $params = array(
-      'name' => 'civicrm_banking.plugin_classes',
-      'version' => 3,
-  );
-  $result = civicrm_api('option_group', 'getsingle', $params);
-  if ($result['is_error']) {
-    $params = array(
-        'version' => 3,
-        'sequential' => 1,
-        'name' => 'civicrm_banking.plugin_classes',
-        'is_reserved' => 1,
-        'is_active' => 1,
-        'title' => 'CiviBanking plugin classes',
-        'description' => 'The set of existing CiviBanking plugin types',
-    );
-    $result = civicrm_api('option_group', 'create', $params);    
-  }
+  banking_civicrm_install_options(banking_civicrm_options());
 
   return _banking_civix_civicrm_install();
 }
 
+function banking_civicrm_install_options($data) {
+  foreach ($data as $groupName => $group) {
+    // check group existence
+    $result = civicrm_api('option_group', 'getsingle', array('version' => 3, 'name' => $groupName));
+    if ($result['is_error']) {
+      $params = array(
+          'version' => 3,
+          'sequential' => 1,
+          'name' => $groupName,
+          'is_reserved' => 1,
+          'is_active' => 1,
+          'title' => $group['title'],
+          'description' => $group['description'],
+      );
+      $result = civicrm_api('option_group', 'create', $params);
+      $group_id = $result['values'][0]['id'];
+    } else
+      $group_id = $result['id'];
+
+    if (is_array($group['values'])) {
+      $groupValues = $group['values'];
+      $weight = 1;
+      print_r(array_keys($groupValues));
+      foreach ($groupValues as $valueName => $value) {
+        $result = civicrm_api('option_value', 'getsingle', array('version' => 3, 'name' => $valueName));
+        if ($result['is_error']) {
+          $params = array(
+              'version' => 3,
+              'sequential' => 1,
+              'option_group_id' => $group_id,
+              'name' => $valueName,
+              'label' => $value['label'],
+              'value' => $value['value'],
+              'weight' => $weight,
+              'is_default' => $value['is_default'],
+              'is_active' => 1,
+          );
+          $result = civicrm_api('option_value', 'create', $params);
+        } else {
+          $weight = $result['weight'] + 1;
+        }
+      }
+    }
+  }
+}
+
+function banking_civicrm_options() {
+  // start with the lowest weight value
+  return array(
+      'civicrm_banking.plugin_types' => array(
+          'title' => 'CiviBanking plugin types',
+          'description' => 'The set of possible CiviBanking plugin types',
+          'values' => array(
+              'import' => array(
+                  'label' => 'Import plugin',
+                  'value' => 1,
+                  'is_default' => 0,
+              ),
+              'match' => array(
+                  'label' => 'Match plugin',
+                  'value' => 2,
+                  'is_default' => 0,
+              ),
+              'export' => array(
+                  'label' => 'Export plugin',
+                  'value' => 3,
+                  'is_default' => 0,
+              ),
+          ),
+      ),
+      'civicrm_banking.plugin_classes' => array(
+          'title' => 'CiviBanking plugin classes',
+          'description' => 'The set of existing CiviBanking plugin types',
+      ),
+      'civicrm_banking.bank_tx_status' => array(
+          'title' => 'CiviBanking bank transaction processing status',
+          'description' => 'The set of possible processing statuses for a CiviBanking bank transaction',
+          'values' => array(
+              'new' => array(
+                  'label' => 'New',
+                  'value' => 0,
+                  'is_default' => 1,
+              ),
+              'ignored' => array(
+                  'label' => 'Ignored',
+                  'value' => 1,
+                  'is_default' => 0,
+              ),
+              'suggestions' => array(
+                  'label' => 'Suggestions',
+                  'value' => 2,
+                  'is_default' => 0,
+              ),
+              'processed' => array(
+                  'label' => 'Processed',
+                  'value' => 3,
+                  'is_default' => 0,
+              ),
+          ),
+      ),
+  );
+}
 
 /**
  * Implementation of hook_civicrm_uninstall
@@ -137,83 +185,83 @@ function banking_civicrm_managed(&$entities) {
  *
  * Insert Banking menu at top level
  */
-function banking_civicrm_navigationMenu( &$params ) {
- 
-    //  Get the maximum key of $params
-    $maxKey = ( max( array_keys($params) ) );
-    $insert_at = min(4, max(array_keys($params)));
- 
-    $banking_entry = array (
-                       'attributes' => array (
-                                              'label'      => 'Banking',
-                                              'name'       => 'CiviBanking',
-                                              'url'        => null,
-                                              'permission' => null,
-                                              'operator'   => null,
-                                              'separator'  => null,
-                                              'parentID'   => null,
-                                              'navID'      => $insert_at,
-                                              'active'     => 1
-                                              ),
-                       'child' =>  array (
-                                          '1' => array (
-                                                        'attributes' => array (
-                                                                               'label'      => 'Payments',
-                                                                               'name'       => 'Payments',
-                                                                               'url'        => 'civicrm/banking/payments',
-                                                                               'permission' => 'access CiviContribute',
-                                                                               'operator'   => null,
-                                                                               'separator'  => 0,
-                                                                               'parentID'   => $insert_at,
-                                                                               'navID'      => 1,
-                                                                               'active'     => 1
-                                                                                ),
-                                                        'child' => null
-                                                        ),
-                                          '2' => array (
-                                                        'attributes' => array (
-                                                                               'label'      => 'Import Payments',
-                                                                               'name'       => 'Import Payments',
-                                                                               'url'        => 'civicrm/banking/import',
-                                                                               'permission' => 'access CiviContribute',
-                                                                               'operator'   => null,
-                                                                               'separator'  => 1,
-                                                                               'parentID'   => $insert_at,
-                                                                               'navID'      => 2,
-                                                                               'active'     => 1
-                                                                                ),
-                                                        'child' => null
-                                                        ),
-                                          '3' => array (
-                                                        'attributes' => array (
-                                                                               'label'      => 'Accounts',
-                                                                               'name'       => 'Accounts',
-                                                                               'url'        => 'civicrm/banking/accounts',
-                                                                               'permission' => 'access CiviContribute',
-                                                                               'operator'   => null,
-                                                                               'separator'  => 1,
-                                                                               'parentID'   => $insert_at,
-                                                                               'navID'      => 3,
-                                                                               'active'     => 1
-                                                                                ),
-                                                        'child' => null
-                                                        ),
-                                          '4' => array (
-                                                        'attributes' => array (
-                                                                               'label'      => 'Manage Components',
-                                                                               'name'       => 'Manage Components',
-                                                                               'url'        => 'civicrm/banking/manager',
-                                                                               'permission' => 'access CiviContribute',
-                                                                               'operator'   => null,
-                                                                               'separator'  => 0,
-                                                                               'parentID'   => $insert_at,
-                                                                               'navID'      => 4,
-                                                                               'active'     => 1
-                                                                                ),
-                                                        'child' => null
-                                                        )
-										)
-							);
+function banking_civicrm_navigationMenu(&$params) {
 
-	$params = array_merge(array_slice($params, 0, $insert_at), array($banking_entry), array_slice($params, $insert_at));
+  //  Get the maximum key of $params
+  $maxKey = ( max(array_keys($params)) );
+  $insert_at = min(4, max(array_keys($params)));
+
+  $banking_entry = array(
+      'attributes' => array(
+          'label' => 'Banking',
+          'name' => 'CiviBanking',
+          'url' => null,
+          'permission' => null,
+          'operator' => null,
+          'separator' => null,
+          'parentID' => null,
+          'navID' => $insert_at,
+          'active' => 1
+      ),
+      'child' => array(
+          '1' => array(
+              'attributes' => array(
+                  'label' => 'Payments',
+                  'name' => 'Payments',
+                  'url' => 'civicrm/banking/payments',
+                  'permission' => 'access CiviContribute',
+                  'operator' => null,
+                  'separator' => 0,
+                  'parentID' => $insert_at,
+                  'navID' => 1,
+                  'active' => 1
+              ),
+              'child' => null
+          ),
+          '2' => array(
+              'attributes' => array(
+                  'label' => 'Import Payments',
+                  'name' => 'Import Payments',
+                  'url' => 'civicrm/banking/import',
+                  'permission' => 'access CiviContribute',
+                  'operator' => null,
+                  'separator' => 1,
+                  'parentID' => $insert_at,
+                  'navID' => 2,
+                  'active' => 1
+              ),
+              'child' => null
+          ),
+          '3' => array(
+              'attributes' => array(
+                  'label' => 'Accounts',
+                  'name' => 'Accounts',
+                  'url' => 'civicrm/banking/accounts',
+                  'permission' => 'access CiviContribute',
+                  'operator' => null,
+                  'separator' => 1,
+                  'parentID' => $insert_at,
+                  'navID' => 3,
+                  'active' => 1
+              ),
+              'child' => null
+          ),
+          '4' => array(
+              'attributes' => array(
+                  'label' => 'Manage Components',
+                  'name' => 'Manage Components',
+                  'url' => 'civicrm/banking/manager',
+                  'permission' => 'access CiviContribute',
+                  'operator' => null,
+                  'separator' => 0,
+                  'parentID' => $insert_at,
+                  'navID' => 4,
+                  'active' => 1
+              ),
+              'child' => null
+          )
+      )
+  );
+
+  $params = array_merge(array_slice($params, 0, $insert_at), array($banking_entry), array_slice($params, $insert_at));
 }

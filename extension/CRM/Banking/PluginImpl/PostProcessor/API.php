cec7b4deed71dd467fbd8ef9ff9eb4fd7e53d6d8
systopia <endres@systopia.de>
2017-07-26 18:56:36 +0200

    implemented #67 API post processor

--- extension/CRM/Banking/PluginImpl/PostProcessor/API.php
+++ extension/CRM/Banking/PluginImpl/PostProcessor/API.php
@@ -16,8 +16,7 @@
 
 
 /**
- * This PostProcessor will connect the generated/matched contribution
- * with a membership
+ * This PostProcessor call an API action if triggered
  */
 class CRM_Banking_PluginImpl_PostProcessor_API extends CRM_Banking_PluginModel_PostProcessor {
 
@@ -30,19 +29,39 @@ class CRM_Banking_PluginImpl_PostProcessor_API extends CRM_Banking_PluginModel_P
     // read config, set defaults
     $config = $this->_plugin_config;
 
-    if (!isset($config->contribution_fields_checked)) $config->contribution_fields_checked = 'id,financial_type_id,total_amount';
-    if (!isset($config->membership_id))               $config->membership_id = 'btx.membership_id';
-    if (!isset($config->financial_type_ids))          $config->financial_type_ids = array(2);
-    if (!isset($config->contribution_status_ids))     $config->contribution_status_ids = array(1);
-    // if (!isset($config->received_date_minimum)) $config->received_date_minimum = "-10 days";
-
-
-     "membership_id": "btx.membership_id",
-   "financial_type_ids": [2],
-   "contribution_status_ids": [1]
-
-
+    if (!isset($config->entity))            $config->entity = NULL;
+    if (!isset($config->action))            $config->action = NULL;
+    if (!isset($config->params))            $config->params = array();
+    if (!isset($config->param_propagation)) $config->param_propagation = array();
   }
+
+  /**
+   * Should this postprocessor spring into action?
+   * Evaluates the common 'required' fields in the configuration
+   *
+   * @param $match    the executed match
+   * @param $btx      the related transaction
+   * @param $context  the matcher context contains cache data and context information
+   *
+   * @return bool     should the this postprocessor be activated
+   */
+  protected function shouldExecute(CRM_Banking_Matcher_Suggestion $match, CRM_Banking_PluginModel_Matcher $matcher, CRM_Banking_Matcher_Context $context) {
+    $config = $this->_plugin_config;
+
+    // check if an entity is set
+    if (empty($config->entity)) {
+      return FALSE;
+    }
+
+    // check if an action is set
+    if (empty($config->action)) {
+      return FALSE;
+    }
+
+    // pass on to parent to check generic reasons
+    return parent::shouldExecute($match, $matcher, $context);
+  }
+
 
   /**
    * Postprocess the (already executed) match
@@ -56,32 +75,31 @@ class CRM_Banking_PluginImpl_PostProcessor_API extends CRM_Banking_PluginModel_P
     $config = $this->_plugin_config;
 
     if ($this->shouldExecute($match, $matcher, $context)) {
-      // TODO: get membership ID
-      $membership_id = 1;
-
-      $contribution_ids = $this->getContributionIDs($context);
-      if (!empty($contribution_ids)) {
-        $contributions = civicrm_api3('Contribution', 'get', array(
-          'id'     => array('IN' => $contribution_ids),
-          'return' =>$config->contribution_fields_checked,
-          ));
-        foreach ($contributions['values'] as $contribution) {
-          if ($this->isContributionEligibleForMembership($contribution)) {
-            civicrm_api3('MembershipPayment', 'create', array(
-              'contribution_id' => $contribution['id'],
-              'membership_id'   => $membership_id,
-              ));
-          }
+      // compile call parameters
+      $params = array();
+      foreach ($config->params as $key => $value) {
+        if ($value !== NULL) {
+          $params[$key] = $value;
         }
       }
+
+      foreach ($config->param_propagation as $value_source => $value_key) {
+        $value = $this->getPropagationValue($context->btx, $match, $value_source);
+        if ($value !== NULL) {
+          $params[$value_key] = $value;
+        }
+      }
+
+      // perform the call
+      try {
+        // TODO: log
+        error_log("CALLING {$config->entity}.{$config->action} with " . json_encode($params));
+        civicrm_api3($config->entity, $config->action, $params);
+      } catch (Exception $e) {
+        // TODO: log
+        error_log("CALLING {$config->entity}.{$config->action} failed: " . $e->getMessage());
+      }
     }
-  }
-
-
-
-  protected function isContributionEligibleForMembership($contribution) {
-    // TODO:
-    return TRUE;
   }
 }
 

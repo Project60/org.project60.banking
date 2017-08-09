00a2ee487812102f045c35be0df3e5c388771f3c
systopia <endres@systopia.de>
2017-08-09 08:35:29 +0200

    added accounts post processor (#67)

---
+++ extension/CRM/Banking/PluginImpl/PostProcessor/Accounts.php
@@ -0,0 +1,177 @@
+<?php
+/*-------------------------------------------------------+
+| Project 60 - CiviBanking                               |
+| Copyright (C) 2017 SYSTOPIA                            |
+| Author: B. Endres (endres -at- systopia.de)            |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL v3 license. You can redistribute it and/or  |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+
+/**
+ * This PostProcessor will store account information after the match
+ */
+class CRM_Banking_PluginImpl_PostProcessor_Accounts extends CRM_Banking_PluginModel_PostProcessor {
+
+  // caches the account values
+  public static $_account_cache = array();
+
+  /**
+   * class constructor
+   */
+  function __construct($config_name) {
+    parent::__construct($config_name);
+
+    // read config, set defaults
+    $config = $this->_plugin_config;
+    if (!isset($config->mode))          $config->mode          = 'both'; // or 'debit' or 'credit'
+    if (!isset($config->type))          $config->type          = 'reference'; // or 'ba_id'
+    if (!isset($config->ref_type))      $config->ref_type      = 'IBAN'; // or any other reference type name
+    if (!isset($config->target))        $config->target        = 'contribution'; // or 'contact'
+    if (!isset($config->own_account))   $config->own_account   = NULL;
+    if (!isset($config->party_account)) $config->party_account = NULL;
+  }
+
+  /**
+   * Should this postprocessor spring into action?
+   * Evaluates the common 'required' fields in the configuration
+   *
+   * @param $match    the executed match
+   * @param $btx      the related transaction
+   * @param $context  the matcher context contains cache data and context information
+   *
+   * @return bool     should the this postprocessor be activated
+   */
+  protected function shouldExecute(CRM_Banking_Matcher_Suggestion $match, CRM_Banking_PluginModel_Matcher $matcher, CRM_Banking_Matcher_Context $context) {
+    $config = $this->_plugin_config;
+    $btx = $context->btx;
+
+    // only be active if there are fields to be written into...
+    if (empty($config->own_account) && empty($config->party_account)) {
+      return FALSE;
+    }
+
+    // check if we're in the right mode
+    switch (strtolower($config->mode)) {
+      case 'debit':
+        if ($btx->amount >= 0.0) {
+          return FALSE;
+        }
+        break;
+
+      case 'credit':
+        if ($btx->amount < 0.0) {
+          return FALSE;
+        }
+        break;
+
+      default:
+      case 'both':
+        // this one always works
+        break;
+    }
+
+    // pass on to parent to check generic reasons
+    return parent::shouldExecute($match, $matcher, $context);
+  }
+
+
+  /**
+   * Postprocess the (already executed) match
+   *
+   * @param $match    the executed match
+   * @param $btx      the related transaction
+   * @param $context  the matcher context contains cache data and context information
+   *
+   */
+  public function processExecutedMatch(CRM_Banking_Matcher_Suggestion $match, CRM_Banking_PluginModel_Matcher $matcher, CRM_Banking_Matcher_Context $context) {
+    $config = $this->_plugin_config;
+
+    // compile update
+    $update = array();
+
+    if (!empty($config->own_account)) {
+      $own_account_reference = $this->getAccountData($context, '_', TRUE);
+      if ($own_account_reference) {
+        $update[$config->own_account] = $own_account_reference;
+      }
+    }
+
+    if (!empty($config->party_account)) {
+      $party_account_reference = $this->getAccountData($context, '_');
+      if ($party_account_reference) {
+        $update[$config->party_account] = $party_account_reference;
+      }
+    }
+
+    if (empty($update)) {
+      // there's nothing to update
+      return;
+    }
+
+    // get the entity ID
+    $object = $this->getPropagationObject($config->target, $context->btx);
+    if (empty($object['id'])) {
+      // TODO: log: object $config->target could not be uniquely identified
+      return;
+    } else {
+      $update['id'] = $object['id'];
+    }
+
+    // execute update to store the bank accounts
+    civicrm_api3($config->target, 'create', $update);
+  }
+
+  /**
+   * get the desired account data to write into the custom fields
+   */
+  protected function getAccountData($context, $prefix, $cache = FALSE) {
+    $data  = $context->btx->getDataParsed();
+    $value = CRM_Utils_Array::value("{prefix}{$config->ref_type}", $data);
+    if (empty($value)) {
+      // no account reference given
+      return NULL;
+    }
+
+    if ($config->type == 'ba_id') {
+      // this means we want the account ID, not just the reference
+      $contact_id = $this->getSoleContactID($context);
+      if (empty($contact_id)) {
+        // we cannot create/find the bank account if there is no contact
+        return NULL;
+      }
+
+      $cache_key = "{$contact_id}-#-{$value}";
+      if ($cache && isset(self::$_account_cache[$cache_key])) {
+        $values = self::$_account_cache[$cache_key];
+      } else {
+        // compile get-or-create
+        $bank_account_data = array(
+          'reference_type' => $config->ref_type,
+          'reference'      => $value,
+          'contact_id'     => $contact_id);
+
+        // special treatment for IBANs
+        if ($config->ref_type == 'IBAN' && !empty($data["{$prefix}BIC"])) {
+          $bank_account_data['bic'] = $data["{$prefix}BIC"];
+        }
+
+        // run the query
+        $bank_account = civicrm_api3('BankingAccount', 'getorcreate', $bank_account_data);
+        $value = $bank_account['id'];
+        if ($cache) {
+          self::$_account_cache[$cache_key] = $value;
+        }
+      }
+    }
+
+    return $value;
+  }
+}
--- extension/api/v3/BankingAccount.php
+++ extension/api/v3/BankingAccount.php
@@ -30,16 +30,16 @@
  *
  * @param  array   $params           (reference ) input parameters
  *
- * @return array (reference )        
+ * @return array (reference )
  * @access public
  */
 function civicrm_api3_banking_account_create($params) {
   return _civicrm_api3_basic_create("CRM_Banking_BAO_BankAccount", $params);
 }
-  
+
 /**
  * Adjust Metadata for Create action
- * 
+ *
  * The metadata is used for setting defaults, documentation & validation
  * @param array $params array or parameters determined by getfields
  */
@@ -56,7 +56,7 @@ function _civicrm_api3_banking_account_create_spec(&$params) {
  *
  * @param  array   $params           (reference ) input parameters
  *
- * @return array (reference )        
+ * @return array (reference )
  * @access public
  */
 function civicrm_api3_banking_account_get($params) {
@@ -68,10 +68,131 @@ function civicrm_api3_banking_account_get($params) {
  *
  * @param  array   $params           (reference ) input parameters
  *
- * @return array (reference )        
+ * @return array (reference )
  * @access public
  */
 function civicrm_api3_banking_account_delete($params) {
   return _civicrm_api3_basic_delete("CRM_Banking_BAO_BankAccount", $params);
 }
 
+
+
+/**
+ * Get or create a bank account for a given contact
+ * @return array (reference )
+ * @access public
+ */
+function civicrm_api3_banking_account_getorcreate($params) {
+  // sort out the reference type
+  if (is_numeric($params['reference_type'])) {
+    $reference_type_id = (int) $params['reference_type'];
+  } else {
+    $reference_type_id = civicrm_api3('OptionValue', 'getvalue', array(
+      'option_group_id' => 'civicrm_banking.reference_types',
+      'name'            => $params['reference_type'],
+      'return'          => 'id'));
+  }
+
+  if (empty($reference_type_id)) {
+    throw new Exception("Cannot process reference type '{$params['reference_type']}'");
+  }
+
+  // first: find any existing accounts
+  $existing_ba_id = CRM_Core_DAO::singleValueQuery("
+    SELECT ba.id AS ba_id
+    FROM civicrm_bank_account ba
+    LEFT JOIN civicrm_bank_account_reference ref ON ba.id = ref.ba_id
+    WHERE ba.contact_id = %1
+      AND ref.reference = %2
+      AND ref.reference_type_id = %3
+      LIMIT 1", array(
+        1 => array($params['contact_id'], 'Integer'),
+        2 => array($params['reference'], 'String'),
+        3 => array($reference_type_id, 'Integer')));
+  if ($existing_ba_id) {
+    // this account exists!
+    return civicrm_api3('BankingAccount', 'getsingle', array('id' => $existing_ba_id));
+
+  } else {
+    // create a new account
+
+    // first: gather some data
+    $bank_account_data = array();
+    foreach (array('description', 'name', 'country') as $attribute) {
+      if (!empty($params[$attribute])) {
+        $bank_account_data[$attribute] = $params[$attribute];
+      }
+    }
+
+    // special treatment for IBANs
+    if ($params['reference_type'] == 'IBAN') {
+      $bank_account_data['country'] = substr($params['reference'], 0, 2);
+      if (!empty($params['bic'])) {
+        $bank_account_data['bic'] = $params['bic'];
+      }
+    }
+
+    // and then create!
+    $new_bank_account = civicrm_api3('BankingAccount', 'create', array(
+      'contact_id'  => $params['contact_id'],
+      'description' => CRM_Utils_Array::value('description', $params),
+      'data_parsed' => json_encode($bank_account_data)));
+
+    $bank_account_reference = civicrm_api3('BankingAccountReference', 'create', array(
+      'reference'         => $params['reference'],
+      'reference_type_id' => $reference_type_id,
+      'ba_id'             => $new_bank_account['id']));
+
+    return civicrm_api3('BankingAccount', 'getsingle', array('id' => $new_bank_account['id']));
+  }
+}
+
+/**
+ * Get or create a bank account for a given contact
+ * @return array (reference )
+ * @access public
+ */
+function _civicrm_api3_banking_account_getorcreate_spec(&$params){
+  $params['reference'] = array(
+    'name'         => 'reference',
+    'title'        => 'Bank Account Reference',
+    'api.required' => 1,
+    'description'  => 'The bank account reference',
+    );
+  $params['contact_id'] = array(
+    'name'         => 'contact_id',
+    'title'        => 'Contact ID',
+    'api.required' => 1,
+    'description'  => 'CiviCRM contact ID',
+    );
+  $params['reference_type'] = array(
+    'name'         => 'reference_type',
+    'title'        => 'Bank Account Reference type',
+    'api.default'  => 'IBAN',
+    'description'  => 'The reference type (e.g. IBAN). This can be the name or id of the OptionValue',
+    );
+  $params['bic'] = array(
+    'name'         => 'bic',
+    'title'        => 'BIC',
+    'api.required' => 0,
+    'description'  => 'Only processed along with IBAN reference types',
+    );
+  $params['description'] = array(
+    'name'         => 'description',
+    'title'        => 'Account Description',
+    'api.required' => 0,
+    'description'  => 'If a new account is created this will be set as a description',
+    );
+  $params['name'] = array(
+    'name'         => 'name',
+    'title'        => 'Account Name',
+    'api.required' => 0,
+    'description'  => 'If a new account is created this will be set as a name',
+    );
+  $params['country'] = array(
+    'name'         => 'country',
+    'title'        => 'Country (two-letter)',
+    'api.required' => 0,
+    'description'  => 'If a new account is created this will be set as a country',
+    );
+}

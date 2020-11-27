441e6b5fdbffc6cf8f638813bda330d7942b3418
B. Zschiedrich <zschiedrich@systopia.de>
2020-11-27 11:27:06 +0100

    [#311] Statement search form stub

---
+++ extension/CRM/Banking/Form/StatementSearch.php
@@ -0,0 +1,30 @@
+<?php
+/*-------------------------------------------------------+
+| Project 60 - CiviBanking                               |
+| Copyright (C) 2020 SYSTOPIA                            |
+| Author: B. Zschiedrich                                 |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL v3 license. You can redistribute it and/or  |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Banking_ExtensionUtil as E;
+
+class CRM_Banking_Form_StatementSearch extends CRM_Core_Form
+{
+    public function buildQuickForm()
+    {
+        parent::buildQuickForm();
+    }
+
+    public function postProcess()
+    {
+        parent::postProcess();
+    }
+}

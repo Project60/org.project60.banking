d4112e86bee186d4a5de9c874dcc172a0ac42bdf
jensschuppe <jensschuppe@users.noreply.github.com>
2022-09-07 15:38:46 +0200

    Replace `getsingle` API action with `get`

--- extension/CRM/Banking/BAO/PluginInstance.php
+++ extension/CRM/Banking/BAO/PluginInstance.php
@@ -48,7 +48,7 @@ class CRM_Banking_BAO_PluginInstance extends CRM_Banking_DAO_PluginInstance {
    */
   static function listInstances($type_name, $enabled_only=TRUE) {
     // find the correct plugin type
-    $import_plugin_type = civicrm_api3('OptionValue', 'getsingle', array(
+    $import_plugin_type = civicrm_api3('OptionValue', 'get', array(
         'name'     => $type_name,
         'option_group_id' => 'civicrm_banking.plugin_classes',
         'option.limit' => 0));

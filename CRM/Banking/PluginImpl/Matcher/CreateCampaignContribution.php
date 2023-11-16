ebda0ee0ea50bd993062878c3612cddc5c394478
B. Endres <endres@systopia.de>
2023-11-16 14:24:36 +0100

    [#296] WIP - implementing

--- CRM/Banking/PluginImpl/Matcher/CreateCampaignContribution.php
+++ CRM/Banking/PluginImpl/Matcher/CreateCampaignContribution.php
@@ -248,7 +248,7 @@ class CRM_Banking_PluginImpl_Matcher_CreateCampaignContribution extends CRM_Bank
     $smarty_vars['activity_title'] = E::ts("'%1' from %2 days earlier", [1 => $activity['subject'], 2 => $days_since_contribution]);
     $smarty_vars['activity_id']    = $activity['id'] ?? 'n/a';
     $smarty_vars['activity_url']   = CRM_Utils_System::url('civicrm/activity/view', "action=view&reset=1&id={$activity['id']}");
-    $smarty_vars['activity_link']  = E::ts('<a class="crm-popup" href="%1" class="crm-popup">%2</a>', [1 => $smarty_vars['activity_url'], 2 => $smarty_vars['activity_title']]);
+    $smarty_vars['activity_link']  = E::ts('<a class="crm-popup" href="%1">%2</a>', [1 => $smarty_vars['activity_url'], 2 => $smarty_vars['activity_title']]);
 
     // add campaign info
     $smarty_vars['campaign_name']       = $campaign['title'];

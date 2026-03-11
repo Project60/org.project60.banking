0f1bc178860fc62f3a91043a40914c72c49d3320
Dominic Tubach <tubach@systopia.de>
2026-03-11 10:11:50 +0100

    Set version to 1.4.0-beta6

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'project60/org.project60.banking',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '836656bdb8bc74bffa0adb0e425f9dd2c23d1408',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.4.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'project60/org.project60.banking' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '836656bdb8bc74bffa0adb0e425f9dd2c23d1408',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.4.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);

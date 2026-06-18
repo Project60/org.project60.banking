01a595791c73725bb72fd9adde0a14dde2c6fc12
Jens Schuppe <schuppe@systopia.de>
2026-06-18 14:08:21 +0200

    Set version to 1.5.0-alpha1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,36 @@
+<?php return array(
+    'root' => array(
+        'name' => 'project60/org.project60.banking',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1d9b781704ea7dc23ec6ed6c01f1889161245787',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.5.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'project60/org.project60.banking' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1d9b781704ea7dc23ec6ed6c01f1889161245787',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.5.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'webmozart/assert' => array(
+            'pretty_version' => '1.12.1',
+            'version' => '1.12.1.0',
+            'reference' => '9be6926d8b485f55b9229203f962b51ed377ba68',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webmozart/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
